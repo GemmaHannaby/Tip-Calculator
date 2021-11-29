3c478ada7829932bc714bc4dc072d57471455b09
GemmaHannaby <68431490+GemmaHannaby@users.noreply.github.com>
2021-11-29 11:14:47 +0000

    Update Tip_calculator.py

--- Tip_calculator.py
+++ Tip_calculator.py
@@ -1,7 +1,3 @@
-#bill is £150.00, split between 5 people with 12% tip
-#Each person should pay (150.00 / 5) * 1.12
-#Round the result to 2 decimal places = 33.60
-
 print("Welcome to the tip calculator!")
 bill = float(input("What was the total bill? £"))
 tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
